107ee54d31ecac7972fca3d889d707cc2ee068a5
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2024-12-05 17:24:44 +0700

    08 middleware basic

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
-const { products } = require('./data')
-app.get('/', (req, res) => {
-  res.json(products)
+
+//  req => middleware => res
+
+const logger = (req, res, next) => {
+  const method = req.method
+  const url = req.url
+  const time = new Date().getFullYear()
+  console.log(method, url, time)
+  next()
+}
+
+app.get('/', logger, (req, res) => {
+  res.send('Home')
+})
+app.get('/about', logger, (req, res) => {
+  res.send('About')
 })
 
 app.listen(5000, () => {
