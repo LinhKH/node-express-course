7167c1b888dbc6f41bc56b3c054673b8cd968040
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2024-12-06 00:05:40 +0700

    10 middlware options

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const logger = require('./logger')
 const authorize = require('./authorize')
 //  req => middleware => res
-app.use([logger, authorize])
 
-// app.use('/api',[logger])
-// api/home/about/products
+// app.use([logger, authorize])
+// app.use(express.static('./public'))
+app.use(morgan('tiny'))
+
 app.get('/', (req, res) => {
-  console.log(req.user)
   res.send('Home')
 })
 app.get('/about', (req, res) => {
