6ef7215485a45e5cc242933af4eef3c32d4c4078
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2024-12-10 23:00:42 +0700

    build to sync data

--- 06.5-jobster-api/final/client/src/pages/Register.js
+++ 06.5-jobster-api/final/client/src/pages/Register.js
@@ -86,7 +86,7 @@ function Register() {
           disabled={isLoading}
           onClick={() =>
             dispatch(
-              loginUser({ email: 'testUser@test.com', password: 'secret' })
+              loginUser({ email: 'admin@gmail.com', password: 'password' })
             )
           }
         >
